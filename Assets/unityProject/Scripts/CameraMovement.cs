9db4f0b4c1af275dc52145f3fdb6ebf7dec934b5
Miles De Wind <miles.dewind@mail.gvsu.edu>
2018-01-12 08:33:51 -0500

    simple movement, first commit

---
+++ Assets/unityProject/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+  public Transform unit1;
+  public float speed;
+
+  //Update is called once per frame
+  void Update()
+  {
+    //Super simple follower
+    transform.position = Vector3.Lerp(transform.position, unit1.position, speed * Time.deltaTime);
+  }
+}
