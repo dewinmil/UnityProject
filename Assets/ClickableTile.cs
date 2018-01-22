d8aa5527670bfbbc3487ef1f864c4a3f626383e7
Danny DeRuiter <deruitda@mail.gvsu.edu>
2018-01-22 17:47:57 -0500

    Have working pathing using Djikstra

---
+++ Assets/ClickableTile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableTile : MonoBehaviour
+{
+
+    public int tileX;
+    public int tileZ;
+    public TileMap map;
+
+    void OnMouseUp()
+    {
+        map.GeneratePathTo(tileX, tileZ);
+    }
+}
---
+++ Assets/Unit.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Unit : MonoBehaviour
+{
+
+    public int tileX;
+    public int tileZ;
+    public TileMap map;
+
+    public List<Node> currentPath = null;
+
+    void Update()
+    {
+        if (currentPath != null)
+        {
+            int currNode = 0;
+
+            while (currNode < currentPath.Count - 1)
+            {
+                Vector3 start = map.TileCoordToWorldCoord(currentPath[currNode].x, currentPath[currNode].z);
+                Vector3 end = map.TileCoordToWorldCoord(currentPath[currNode + 1].x, currentPath[currNode + 1].z); 
+                Debug.DrawLine(start, end, Color.red);
+
+                currNode++;
+            }
+        }
+    }
+
+    public void MoveToNextTile()
+    {
+        if (currentPath == null)
+            return;
+
+        //remove the old current/first node from the path
+        currentPath.RemoveAt(0);
+        
+        //grab the new first node and move to that position
+        this.transform.position = map.TileCoordToWorldCoord(currentPath[0].x, currentPath[0].z);
+
+        //if we get in this, we know we are at our destination
+        if (currentPath.Count == 1)
+            currentPath = null;
+    }
+}
