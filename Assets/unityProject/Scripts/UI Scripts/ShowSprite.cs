74ee7043cd4ca99868d4fd5da4c55a14991d04b1
Dewinmil <Dewinmil@mail.gvsu.edu>
2018-01-23 16:29:44 -0500

    created a sprite to show selected unit

---
+++ Assets/unityProject/Scripts/UI Scripts/ShowSprite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowSprite : MonoBehaviour {
+
+    public MoveInput character;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (character.isSelected == true)
+        {
+            if (gameObject.GetComponent<SpriteRenderer>().enabled == false)
+            {
+                gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+
+    }
+}
