e302c98158719d413c76573325cc61ab1c43fa88
shoupdy <shoupdy@mail.gvsu.edu>
2018-01-25 08:40:58 -0500

    Merge branch 'master' of https://github.com/dewinmil/UnityProject into dylan

---
+++ Assets/unityProject/Scripts/Character Scripts/MoveInput.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInput : MonoBehaviour {
+
+  public Transform unit1Pointer;
+  public unitMovement unit1Movement;
+  public float minMovRange;
+  public SpriteRenderer cursor1;
+  public Ray ray;
+  public bool isSelected;
+	
+  // Update is called once per frame
+  void Update () {
+    //Get Input
+    if (Input.GetButton("Fire1"))
+    {
+      ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit;
+      if (Physics.Raycast(ray, out hit, 100))
+      {
+        //Debugging Ray
+        Debug.DrawLine(ray.origin, hit.point);
+          
+        //Move The Pointer
+        Vector3 selectedPosition = new Vector3(hit.point.x, unit1Pointer.position.y, hit.point.z);
+
+        if (Vector3.Distance(selectedPosition, unit1Movement.transform.position) <= minMovRange)
+        {
+          isSelected = true;
+        }
+        else
+        {
+          isSelected = false;
+        }
+      }
+    }
+    if (Input.GetButton("Fire2"))
+    {
+      //Cast ray to get position of cursor on Terrain
+      ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit;
+      if (isSelected == true)
+      {
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+          if (hit.collider.tag == "Terrain")
+          {
+            //Debugging Ray
+            Debug.DrawLine(ray.origin, hit.point);
+          
+            //Move The Pointer
+            unit1Pointer.position = new Vector3(hit.point.x, unit1Pointer.position.y, hit.point.z);
+
+            //Only moves if the distance between the pointer and the unit is big enough
+            if (Vector3.Distance(unit1Pointer.position, unit1Movement.transform.position) > minMovRange)
+            {
+              unit1Movement.moving = true;
+              cursor1.enabled = true;
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ Assets/unityProject/Scripts/Character Scripts/unitMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; //Contains NavMeshClasses
+
+public class unitMovement : MonoBehaviour
+{
+
+    Animator anim;
+    public bool moving;
+    public NavMeshAgent agent;
+    public Transform pointer;
+
+    // Update is called once per frame
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        //Activates the NavMeshAgent Movement
+        anim.SetBool("Moving", moving);
+        if (moving)
+        {
+            
+            agent.SetDestination(pointer.position);
+            agent.isStopped = false;
+            //agent.Resume();
+        }
+    }
+}
