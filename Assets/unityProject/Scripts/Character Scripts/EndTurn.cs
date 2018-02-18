b0d3f0db12b869a5e7ed7f37fc0c7f0259f88461
DESKTOP-PN6HJ26\adamc <caldwead@mail.gvsu.edu>
2018-02-18 14:35:10 -0500

    Updated EndTurn and created GameMaster class

--- Assets/unityProject/Scripts/Character Scripts/EndTurn.cs
+++ Assets/unityProject/Scripts/Character Scripts/EndTurn.cs
@@ -8,12 +8,15 @@ public class EndTurn : MonoBehaviour
     public CharacterStatus unitStatus;
     public MoveInput unitMoveInput;
     public Abilities currUnit;
-    public List<Node> turnUsed = new List<Node>();
+    //public int currTeam;
+    public List<Abilities> turnUsed = new List<Abilities>();
+    public List<Abilities> unitList = new List<Abilities>();
 
     // Use this for initialization
     void Start()
     {
         this.currUnit._unit.unitId = 0;
+    //    currTeam = 0;
     }
 
     // Update is called once per frame
@@ -22,13 +25,30 @@ public class EndTurn : MonoBehaviour
 
     }
 
-    public void switchTeam()
+    public void switchTeam(int currTeam)
     {
+        foreach (Abilities unit in unitList)
+        {
+            if (unit._casterStatus.teamNum == currTeam)
+            {
+                unit._casterStatus.gainAction();
+            }
+        }
         // switch control to other player.
+        currTeam = (currTeam + 1) % 2;
     }
 
-    public void switchUnit()
+    public void switchUnit(int currTeam)
     {
         // switch control to different unit on the same team.
+        turnUsed.Add(currUnit);
+
+        foreach (Abilities unit in unitList)
+        {
+            if (unit._casterStatus.teamNum == currTeam && !turnUsed.Contains(unit))
+            {
+                currUnit = unit;
+            }
+        }
     }
 }
