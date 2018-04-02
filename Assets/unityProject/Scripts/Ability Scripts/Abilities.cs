8bbce2c8817f7ffe151f60950e33d184b6cfd083
shoupdy <shoupdy@mail.gvsu.edu>
2018-04-02 19:16:52 -0400

    Melee animation and other character fixes.

--- Assets/unityProject/Scripts/Ability Scripts/Abilities.cs
+++ Assets/unityProject/Scripts/Ability Scripts/Abilities.cs
@@ -61,6 +61,7 @@ public class Abilities : MonoBehaviour
     public void castAbility(CharacterStatus target, float damage, float healing, float apCost, float armorPen, float magicPen, float range, bool isMagic)
     {
         _unit.transform.LookAt(target.transform.position);
+        target.transform.LookAt(_unit.transform.position);
         //if the target is not dead
         if (target.currentHealth > 0)
         {
