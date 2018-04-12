c982153e96d35e0dde31503f702f7f3b0c5b1d20
dewinmil <dewinmil@mail.gvsu.edu>
2018-04-11 21:42:56 -0400

    removed race condition

--- Assets/unityProject/Scripts/Character Scripts/MoveInput.cs
+++ Assets/unityProject/Scripts/Character Scripts/MoveInput.cs
@@ -21,120 +21,127 @@ public class MoveInput : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isLocalPlayer)
+        if (endTurn == null)
         {
-            //deselect unit if it is not their turn
-            if (endTurn.turn != gameObject.GetComponent<CharacterStatus>().teamNum
-                || gameObject.GetComponent<CharacterStatus>().currentHealth < 0)
+            endTurn = FindObjectOfType<EndTurn>();
+        }
+        else
+        {
+            if (isLocalPlayer)
             {
-                //select the unit
-                isSelected = false;
-                _unit.SelectedUnitChanged();
-            }
-
-            //check whether a spell has been cast upon the owner of this script and if so
-            //ensure that they were not selected as the spell was cast
-            if (targetedBySpell)
-            {
-                isSelected = false;
-            }
-
-            //if the left mouse button is pressed
-            if (Input.GetButtonUp("Fire1"))
-            {
-                //if the cursor is not over a UI element
-                if (EventSystem.current.IsPointerOverGameObject() == false)
+                //deselect unit if it is not their turn
+                if (endTurn.turn != gameObject.GetComponent<CharacterStatus>().teamNum
+                    || gameObject.GetComponent<CharacterStatus>().currentHealth < 0)
                 {
-                    //cast a ray from the main camera to the cursor
-                    ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                    RaycastHit hit;
-                    if (Physics.Raycast(ray, out hit, 100))
-                    {
-                        Debug.DrawLine(ray.origin, hit.point);
-                        Vector3 selectedPosition = new Vector3(hit.point.x, hit.point.y - hit.point.y - .416f, hit.point.z);
+                    //select the unit
+                    isSelected = false;
+                    _unit.SelectedUnitChanged();
+                }
 
-                        //if we did selected an area right on top of / near the owner of this script
-                        if (Vector3.Distance(selectedPosition, _characterAbilities.transform.position) <= .9)
+                //check whether a spell has been cast upon the owner of this script and if so
+                //ensure that they were not selected as the spell was cast
+                if (targetedBySpell)
+                {
+                    isSelected = false;
+                }
+
+                //if the left mouse button is pressed
+                if (Input.GetButtonUp("Fire1"))
+                {
+                    //if the cursor is not over a UI element
+                    if (EventSystem.current.IsPointerOverGameObject() == false)
+                    {
+                        //cast a ray from the main camera to the cursor
+                        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                        RaycastHit hit;
+                        if (Physics.Raycast(ray, out hit, 100))
                         {
-                            //if they are not having an ability cast upon them
-                            if (targetedBySpell == false)
+                            Debug.DrawLine(ray.origin, hit.point);
+                            Vector3 selectedPosition = new Vector3(hit.point.x, hit.point.y - hit.point.y - .416f, hit.point.z);
+
+                            //if we did selected an area right on top of / near the owner of this script
+                            if (Vector3.Distance(selectedPosition, _characterAbilities.transform.position) <= .9)
                             {
-                                if (endTurn.turn == gameObject.GetComponent<CharacterStatus>().teamNum
-                                    && gameObject.GetComponent<CharacterStatus>().currentHealth > 0)
+                                //if they are not having an ability cast upon them
+                                if (targetedBySpell == false)
                                 {
-                                    //select the unit
-                                    isSelected = true;
-                                    _unit.SelectedUnitChanged();
-                                    _unit._map.charSelect = true;
+                                    if (endTurn.turn == gameObject.GetComponent<CharacterStatus>().teamNum
+                                        && gameObject.GetComponent<CharacterStatus>().currentHealth > 0)
+                                    {
+                                        //select the unit
+                                        isSelected = true;
+                                        _unit.SelectedUnitChanged();
+                                        _unit._map.charSelect = true;
+                                    }
+                                }
+                                //someone just cast an ability on this unit - do not select this unit
+                                //and the spell has been cast so we are no longer being targeted by an ability
+                                else
+                                {
+                                    targetedBySpell = false;
                                 }
                             }
-                            //someone just cast an ability on this unit - do not select this unit
-                            //and the spell has been cast so we are no longer being targeted by an ability
+                            //if we selected a position away from the unit possessing this script
                             else
                             {
-                                targetedBySpell = false;
-                            }
-                        }
-                        //if we selected a position away from the unit possessing this script
-                        else
-                        {
-                            //if the owner of this scrip is selected
-                            if (isSelected)
-                            {
-                                //if we selected a different unit
-                                if (hit.collider.tag == "Unit")
+                                //if the owner of this scrip is selected
+                                if (isSelected)
                                 {
-                                    //if we are casting a ability
-                                    if (castingSpell)
+                                    //if we selected a different unit
+                                    if (hit.collider.tag == "Unit")
                                     {
-                                        //inform targeted unit that it is being casted upon so that it
-                                        //does not set itself as selected
-                                        hit.collider.gameObject.GetComponent<MoveInput>().targetedBySpell = true;
+                                        //if we are casting a ability
+                                        if (castingSpell)
+                                        {
+                                            //inform targeted unit that it is being casted upon so that it
+                                            //does not set itself as selected
+                                            hit.collider.gameObject.GetComponent<MoveInput>().targetedBySpell = true;
 
-                                        //we finished casting our ability
-                                        castingSpell = false;
+                                            //we finished casting our ability
+                                            castingSpell = false;
+                                        }
+                                        //if we were not casting an ability
+                                        else
+                                        {
+                                            //if we were not attempting to move
+                                            if (_unit._isMoving == false)
+                                            {
+                                                //de-select this unit
+                                                isSelected = false;
+                                                _unit.UnhighlightWalkableTiles();
+                                            }
+                                        }
                                     }
-                                    //if we were not casting an ability
+                                    //if we selcted the ground somewhere
                                     else
                                     {
-                                        //if we were not attempting to move
-                                        if (_unit._isMoving == false)
+                                        //if we were not using an ability
+                                        if (castingSpell == false)
                                         {
-                                            //de-select this unit
-                                            isSelected = false;
+                                            //if we were not trying to move
+                                            if (_unit._isMoving == false)
+                                            {
+                                                //de-select this unit
+                                                isSelected = false;
+                                                _unit._map.charSelect = false;
+                                            }
+                                        }
+                                        //if we were casting an ability
+                                        else
+                                        {
+                                            //de-select the ability
+                                            castingSpell = false;
                                             _unit.UnhighlightWalkableTiles();
                                         }
                                     }
-                                }
-                                //if we selcted the ground somewhere
-                                else
-                                {
-                                    //if we were not using an ability
-                                    if (castingSpell == false)
-                                    {
-                                        //if we were not trying to move
-                                        if (_unit._isMoving == false)
-                                        {
-                                            //de-select this unit
-                                            isSelected = false;
-                                            _unit._map.charSelect = false;
-                                        }
-                                    }
-                                    //if we were casting an ability
-                                    else
-                                    {
-                                        //de-select the ability
-                                        castingSpell = false;
-                                        _unit.UnhighlightWalkableTiles();
-                                    }
+
                                 }
 
                             }
-
                         }
                     }
-                }
 
+                }
             }
         }
     }
